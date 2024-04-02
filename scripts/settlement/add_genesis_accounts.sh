d76ee498a4edd152440d12e5f47d6262af5150ce
artpav <19916123+artemijspavlovs@users.noreply.github.com>
2024-04-02 19:41:16 +0300

    chore: update scripts for initializing evm rollapp (#127)
    Co-authored-by: Omri <omritoptix@gmail.com>

---
+++ scripts/settlement/add_genesis_accounts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ ! -d "$ROLLAPP_SETTLEMENT_INIT_DIR_PATH" ]; then
+  mkdir -p "$ROLLAPP_SETTLEMENT_INIT_DIR_PATH"
+  echo "Creating the ROLLAPP_SETTLEMENT_INIT_DIR_PATH: $ROLLAPP_SETTLEMENT_INIT_DIR_PATH"
+else
+  echo "ROLLAPP_SETTLEMENT_INIT_DIR_PATH already exists: $ROLLAPP_SETTLEMENT_INIT_DIR_PATH"
+fi
+
+dymd keys add alice-genesis --keyring-backend test
+dymd keys add bob-genesis --keyring-backend test
+
+tee "$ROLLAPP_SETTLEMENT_INIT_DIR_PATH/genesis_accounts.json" >/dev/null <<EOF
+[
+  {"amount":
+      {"amount":"10000000000000000000000","denom":"a${DENOM}"},
+      "address": "$(dymd keys show -a bob-genesis --keyring-backend test)"
+  },
+  {"amount":
+      {"amount":"50000000000000000000000","denom":"a${DENOM}"},
+      "address":"$(dymd keys show -a alice-genesis --keyring-backend test)"
+    }
+]
+EOF
