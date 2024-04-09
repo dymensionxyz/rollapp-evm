d5a0f5e47d01e326cbca0de953f224c2c2952186
danwt <30197399+danwt@users.noreply.github.com>
2024-04-09 17:21:58 +0100

    adds a clarification to register rollapp to hub script

--- scripts/settlement/register_rollapp_to_hub.sh
+++ scripts/settlement/register_rollapp_to_hub.sh
@@ -2,9 +2,6 @@
 
 MAX_SEQUENCERS=5
 
-# this account must be whitelisted on the hub for permissioned deployment setup
-DEPLOYER=${HUB_PERMISSIONED_KEY-"$HUB_KEY_WITH_FUNDS"}
-
 if [ -z "$HUB_RPC_URL" ]; then
   echo "HUB_RPC_URL is not set, using 'http://localhost:36657'"
   HUB_RPC_URL="http://localhost:36657"
@@ -14,6 +11,9 @@ if [ -z "$HUB_CHAIN_ID" ]; then
   echo "HUB_CHAIN_ID is not set, using 'dymension_100-1'"
   HUB_CHAIN_ID="dymension_100-1"
 fi
+
+# this account must be whitelisted on the hub for permissioned deployment setup
+DEPLOYER=${HUB_PERMISSIONED_KEY-"$HUB_KEY_WITH_FUNDS"} # if doesn't work see `dymd q rollapp params --node ${HUB_RPC_URL}` (local-user)
 
 # this file is generated using the scripts/settlement/generate_denom_metadata.sh
 DENOM_METADATA_PATH="$HOME/.rollapp_evm/init/denommetadata.json"
