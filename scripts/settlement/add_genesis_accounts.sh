466d622fa577b163b555005b770ddd905aed4e27
danwt <30197399+danwt@users.noreply.github.com>
2024-04-12 09:05:14 +0100

    remove extra keyring stuff from add genesis accounts script

--- scripts/settlement/add_genesis_accounts.sh
+++ scripts/settlement/add_genesis_accounts.sh
@@ -7,18 +7,18 @@ else
   echo "ROLLAPP_SETTLEMENT_INIT_DIR_PATH already exists: $ROLLAPP_SETTLEMENT_INIT_DIR_PATH"
 fi
 
-dymd keys add alice-genesis --keyring-backend test --keyring-dir ${ROLLAPP_HOME_DIR}
-dymd keys add bob-genesis --keyring-backend test --keyring-dir ${ROLLAPP_HOME_DIR}
+dymd keys add alice-genesis
+dymd keys add bob-genesis
 
 tee "$ROLLAPP_SETTLEMENT_INIT_DIR_PATH/genesis_accounts.json" >/dev/null <<EOF
 [
   {"amount":
       {"amount":"10000000000000000000000","denom":"${BASE_DENOM}"},
-      "address": "$(dymd keys show -a bob-genesis --keyring-backend test --keyring-dir ${ROLLAPP_HOME_DIR})"
+      "address": "$(dymd keys show -a bob-genesis)"
   },
   {"amount":
       {"amount":"50000000000000000000000","denom":"${BASE_DENOM}"},
-      "address":"$(dymd keys show -a alice-genesis --keyring-backend test --keyring-dir ${ROLLAPP_HOME_DIR})"
+      "address":"$(dymd keys show -a alice-genesis)"
     }
 ]
 EOF
