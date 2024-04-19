1f27e884ae77bf6730cd852d4ef7dab0053a7919
Sergi Rene <sergi@dymension.xyz>
2024-04-19 18:16:23 +0200

    gas prices in rollapp genesis event script

--- scripts/trigger_rollapp_genesis_event.sh
+++ scripts/trigger_rollapp_genesis_event.sh
@@ -2,5 +2,5 @@
 
 echo "Triggering genesis event on rollapp"
 ROLLAPP_CHANNEL_ID=$(rly q channels "$ROLLAPP_CHAIN_ID" | jq -r 'select(.state == "STATE_OPEN") | .channel_id' | tail -n 1)
-"$EXECUTABLE" tx hubgenesis genesis-event "$HUB_CHAIN_ID" "$ROLLAPP_CHANNEL_ID" --from "$KEY_NAME_ROLLAPP" --keyring-backend test --broadcast-mode block
+"$EXECUTABLE" tx hubgenesis genesis-event "$HUB_CHAIN_ID" "$ROLLAPP_CHANNEL_ID" --from "$KEY_NAME_ROLLAPP" --keyring-backend test --gas-prices 1000000000"$BASE_DENOM" --broadcast-mode block
 
