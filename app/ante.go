3c19252a7bea9947767fb3962f4082ec32853a75
danwt <30197399+danwt@users.noreply.github.com>
2024-04-26 13:37:04 +0100

    factor out MustCreateAnteHandler

--- app/ante.go
+++ app/ante.go
@@ -2,13 +2,54 @@ package app
 
 import (
 	errorsmod "cosmossdk.io/errors"
+	"github.com/cosmos/cosmos-sdk/client"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
+	ethante "github.com/evmos/ethermint/app/ante"
+	ethermint "github.com/evmos/ethermint/types"
+	evmtypes "github.com/evmos/ethermint/x/evm/types"
 
 	ibcante "github.com/cosmos/ibc-go/v6/modules/core/ante"
 	ibckeeper "github.com/cosmos/ibc-go/v6/modules/core/keeper"
 )
+
+func MustCreateAnteHandler(
+	accountKeeper evmtypes.AccountKeeper,
+	bankKeeper evmtypes.BankKeeper,
+	iBCKeeper *ibckeeper.Keeper,
+	feeMarketKeeper ethante.FeeMarketKeeper,
+	evmKeeper ethante.EVMKeeper,
+	feeGrantKeeper ante.FeegrantKeeper,
+	txConfig client.TxConfig,
+	maxGasWanted uint64,
+) sdk.AnteHandler {
+	options := ethante.HandlerOptions{
+		AccountKeeper:          accountKeeper,
+		BankKeeper:             bankKeeper,
+		SignModeHandler:        txConfig.SignModeHandler(),
+		EvmKeeper:              evmKeeper,
+		FeegrantKeeper:         feeGrantKeeper,
+		IBCKeeper:              iBCKeeper,
+		FeeMarketKeeper:        feeMarketKeeper,
+		SigGasConsumer:         ethante.DefaultSigVerificationGasConsumer,
+		MaxTxGasWanted:         maxGasWanted,
+		ExtensionOptionChecker: ethermint.HasDynamicFeeExtensionOption,
+		TxFeeChecker:           ethante.NewDynamicFeeChecker(evmKeeper),
+		DisabledAuthzMsgs: []string{
+			sdk.MsgTypeURL(&evmtypes.MsgEthereumTx{}),
+			sdk.MsgTypeURL(&vestingtypes.MsgCreateVestingAccount{}),
+			sdk.MsgTypeURL(&vestingtypes.MsgCreatePeriodicVestingAccount{}),
+			sdk.MsgTypeURL(&vestingtypes.MsgCreatePermanentLockedAccount{}),
+		},
+	}
+	handler, err := ethante.NewAnteHandler(options)
+	if err != nil {
+		panic(err)
+	}
+	return handler
+}
 
 // HandlerOptions are the options required for constructing a default SDK AnteHandler.
 type HandlerOptions struct {
@@ -50,7 +91,7 @@ func GetAnteDecorators(options HandlerOptions) []sdk.AnteDecorator {
 // numbers, checks signatures & account numbers, and deducts fees from the first
 // signer.
 func NewAnteHandler(options HandlerOptions) (sdk.AnteHandler, error) {
-	//From x/auth/ante.go
+	// From x/auth/ante.go
 	if options.AccountKeeper == nil {
 		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "account keeper is required for ante builder")
 	}
