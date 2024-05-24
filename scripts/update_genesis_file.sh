493ff3996e545cbaad5b7092810da23f6ba390bf
danwt <30197399+danwt@users.noreply.github.com>
2024-05-24 17:03:37 +0100

    try out dasel in the genesis

--- scripts/update_genesis_file.sh
+++ scripts/update_genesis_file.sh
@@ -2,7 +2,6 @@
 # TODO: utils genesis transformer could be used to update the genesis file
 
 set -x
-tmp=$(mktemp)
 CONFIG_DIRECTORY="$ROLLAPP_HOME_DIR/config"
 GENESIS_FILE="$CONFIG_DIRECTORY/genesis.json"
 DYMINT_CONFIG_FILE="$CONFIG_DIRECTORY/dymint.toml"
@@ -10,32 +9,25 @@ APP_CONFIG_FILE="$CONFIG_DIRECTORY/app.toml"
 
 "$EXECUTABLE" keys add hub_genesis --keyring-backend test
 
-jq '.consensus_params["block"]["max_gas"] = "400000000"' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
-jq '.consensus_params["block"]["max_bytes"] = "3145728"' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
+dasel put string -f "$GENESIS_FILE" -p json 'consensus_params.block.max_gas' '400000000'
+dasel put string -f "$GENESIS_FILE" -p json 'consensus_params.block.max_bytes' '3145728'
 
-jq '.app_state.gov.voting_params.voting_period = "300s"' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
+dasel put string -f "$GENESIS_FILE" -p json 'app_state.gov.voting_params.voting_period' '300s'
 
-jq '.app_state.bank.balances[0].coins[0].amount = "2000000000000000000000000000"' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
-jq '.app_state.bank.supply[0].amount = "2000000000000000000000000000"' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
+dasel put string -f "$GENESIS_FILE" -p json 'app_state.bank.balances.0.coins.0.amount' '2000000000000000000000000000'
+dasel put string -f "$GENESIS_FILE" -p json 'app_state.bank.supply.0.amount' '2000000000000000000000000000'
 
 # ---------------------------- add genesis accounts for the hub ---------------------------- #
 genesis_accounts=$(cat "$ROLLAPP_SETTLEMENT_INIT_DIR_PATH"/genesis_accounts.json)
-jq --argjson genesis_accounts "$genesis_accounts" '.app_state.hubgenesis.state.genesis_accounts = $genesis_accounts' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
+dasel put value -f "$GENESIS_FILE" -p json 'app_state.hubgenesis.state.genesis_accounts' -v "$genesis_accounts"
 
 # ---------------------------- add elevated account ---------------------------- # TODO: can I remove it?
-elevated_address=$("$EXECUTABLE" keys show "$KEY_NAME_ROLLAPP" --keyring-backend test --output json | jq -r .address)
-elevated_address_json=$(jq -n \
-  --arg address "$elevated_address" \
-  '[{
-        "address": $address
-    }]')
-jq --argjson elevated_address_json "$elevated_address_json" '.app_state.hubgenesis.params.genesis_triggerer_allowlist += $elevated_address_json' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
+elevated_address=$("$EXECUTABLE" keys show "$KEY_NAME_ROLLAPP" --keyring-backend test --output json | dasel -r json -p json '.address')
 
 # ---------------------------- add denom metadata ---------------------------- #
 denom_metadata=$(cat "$ROLLAPP_SETTLEMENT_INIT_DIR_PATH"/denommetadata.json)
-jq --argjson denom_metadata "$denom_metadata" '.app_state.bank.denom_metadata = $denom_metadata' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
-jq --arg elevated_address "$elevated_address" '.app_state.denommetadata.params.allowed_addresses += [$elevated_address]' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
+dasel put value -f "$GENESIS_FILE" -p json 'app_state.bank.denom_metadata' -v "$denom_metadata"
+dasel put string -f "$GENESIS_FILE" -p json 'app_state.denommetadata.params.allowed_addresses' -v "$elevated_address" --append
 
 # ----------------------------- update evm params ---------------------------- #
-
-jq '.app_state.evm.params.extra_eips = [3855]' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
+dasel put value -f "$GENESIS_FILE" -p json 'app_state.evm.params.extra_eips' -v '[3855]'
