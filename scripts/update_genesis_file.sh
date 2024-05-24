94dcf1767d6985a50fdb715a1757512b408fe418
danwt <30197399+danwt@users.noreply.github.com>
2024-05-24 16:37:45 +0100

    removes the module account funding from the genesis file

--- scripts/update_genesis_file.sh
+++ scripts/update_genesis_file.sh
@@ -15,46 +15,8 @@ jq '.consensus_params["block"]["max_bytes"] = "3145728"' "$GENESIS_FILE" >"$tmp"
 
 jq '.app_state.gov.voting_params.voting_period = "300s"' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
 
-# this is a static module account for the hubgenesis module
-# retrieved using  'rollapp-evm q auth module-accounts' command
-module_account_address=$(rollapp-evm debug addr F54EBEEF798CA51615C02D13888768F9960863F2 | grep "Bech32 Acc" | awk '{print $3}')
-
-# Construct the JSON object with the obtained address
-module_account=$(jq -n \
-  --arg address "$module_account_address" \
-  '[{
-      "@type": "/cosmos.auth.v1beta1.ModuleAccount",
-      "base_account": {
-          "account_number": "0",
-          "address": $address,
-          "pub_key": null,
-          "sequence": "0"
-      },
-      "name": "hubgenesis",
-      "permissions": []
-  }]')
-
-jq --argjson module_account "$module_account" '.app_state.auth.accounts += $module_account' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
-
-module_account_balance=$(
-  jq -n \
-    --arg address "$module_account_address" \
-    --arg denom "$BASE_DENOM" \
-    '[{
-    "address": $address,
-    "coins": [
-      {
-        "denom": $denom,
-        "amount": "60000000000000000000000"
-      }
-    ]
-  }]'
-)
-
 jq '.app_state.bank.balances[0].coins[0].amount = "2000000000000000000000000000"' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
-jq --argjson module_account_balance "$module_account_balance" '.app_state.bank.balances += $module_account_balance' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
-
-jq '.app_state.bank.supply[0].amount = "2000060000000000000000000000"' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
+jq '.app_state.bank.supply[0].amount = "2000000000000000000000000000"' "$GENESIS_FILE" >"$tmp" && mv "$tmp" "$GENESIS_FILE"
 
 # ---------------------------- add genesis accounts for the hub ---------------------------- #
 genesis_accounts=$(cat "$ROLLAPP_SETTLEMENT_INIT_DIR_PATH"/genesis_accounts.json)
