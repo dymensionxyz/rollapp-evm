8c9de228299ad29224e51d086786ef3f3abf3d02
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-07-09 20:36:25 +0300

    fix(scripts): use the keyring backend from the configuration

--- scripts/settlement/register_rollapp_to_hub.sh
+++ scripts/settlement/register_rollapp_to_hub.sh
@@ -5,12 +5,12 @@ MAX_SEQUENCERS=5
 # this account must be whitelisted on the hub for permissioned deployment setup
 DEPLOYER=${HUB_PERMISSIONED_KEY-"$HUB_KEY_WITH_FUNDS"}
 
-if [ -z "$HUB_RPC_URL" ]; then
+if [ "$HUB_RPC_URL" = "" ]; then
   echo "HUB_RPC_URL is not set, using 'http://localhost:36657'"
   HUB_RPC_URL="http://localhost:36657"
 fi
 
-if [ -z "$HUB_CHAIN_ID" ]; then
+if [ "$HUB_CHAIN_ID" = "" ]; then
   echo "HUB_CHAIN_ID is not set, using 'dymension_100-1'"
   HUB_CHAIN_ID="dymension_100-1"
 fi
@@ -19,7 +19,7 @@ set -x
 #Register rollapp
 dymd tx rollapp create-rollapp "$ROLLAPP_CHAIN_ID" "$MAX_SEQUENCERS" '{"Addresses":[]}' \
   --from "$DEPLOYER" \
-  --keyring-backend test \
+  --keyring-backend "$(dymd config keyring-backend)" \
   --broadcast-mode block \
   --fees 1dym \
   -y
