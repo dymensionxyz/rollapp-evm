e092d90e665adc62143d66be2412100158efb69c
artpav <19916123+artemijspavlovs@users.noreply.github.com>
2024-10-23 17:56:18 +0300

    chore: update minimal genesis script to remove accounts (#355)

--- scripts/minimal_update_genesis_params.sh
+++ scripts/minimal_update_genesis_params.sh
@@ -17,10 +17,14 @@ update_params() {
   dasel put -t bool -f "$GENESIS_FILE" 'app_state.feemarket.params.no_base_fee' -v false || success=false
   dasel put -f "$GENESIS_FILE" '.app_state.evm.params.extra_eips.[]' -v '3855' || success=false
   dasel put -f "$GENESIS_FILE" '.app_state.feemarket.params.min_gas_price' -v "10000000.0" || success=false
-  dasel put -f "$GENESIS_FILE" 'app_state.distribution.params.base_proposer_reward' -v '0.8' || success=false
-  dasel put -f "$GENESIS_FILE" 'app_state.distribution.params.community_tax' -v "0.00002" || success=false
+  dasel put -f "$GENESIS_FILE" '.app_state.distribution.params.base_proposer_reward' -v '0.8' || success=false
+  dasel put -f "$GENESIS_FILE" '.app_state.distribution.params.community_tax' -v "0.00002" || success=false
+  dasel put -f "$GENESIS_FILE" '.app_state.auth.accounts' -t json -r json -v '[]' || success=false # 2 weeks
+  dasel put -f "$GENESIS_FILE" '.app_state.bank.balances' -t json -r json -v '[]' || success=false # 2 weeks
+  dasel put -f "$GENESIS_FILE" '.app_state.genutil.gen_txs' -t json -r json -v '[]' || success=false # 2 weeks
+
   # these vary depending on environment
-  dasel put -f "$GENESIS_FILE" 'app_state.gov.voting_params.voting_period' -v "300s" || success=false
+  dasel put -f "$GENESIS_FILE" '.app_state.gov.voting_params.voting_period' -v "300s" || success=false
   dasel put -f "$GENESIS_FILE" '.app_state.sequencers.params.unbonding_time' -v "1209600s" || success=false # 2 weeks
   dasel put -f "$GENESIS_FILE" '.app_state.staking.params.unbonding_time' -v "1209600s" || success=false # 2 weeks
 
