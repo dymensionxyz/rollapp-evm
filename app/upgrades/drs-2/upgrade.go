950d2ba2b1eb6c616e9ab99ebbc6a49b65326fdd
Sergi Rene <sergi@dymension.xyz>
2024-12-12 14:47:03 +0100

    feat(upgrade): add drs 2 upgrade handler (#396)
    Co-authored-by: omritoptix <omritoptix@gmail.com>
    Co-authored-by: keruch <verendeev.kirill.2000@gmail.com>

---
+++ app/upgrades/drs-2/upgrade.go
@@ -0,0 +1,39 @@
+package drs2
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	rollappparamskeeper "github.com/dymensionxyz/dymension-rdk/x/rollappparams/keeper"
+	rollappparamstypes "github.com/dymensionxyz/dymension-rdk/x/rollappparams/types"
+	evmkeeper "github.com/evmos/evmos/v12/x/evm/keeper"
+	evmtypes "github.com/evmos/evmos/v12/x/evm/types"
+)
+
+func CreateUpgradeHandler(
+	rpKeeper rollappparamskeeper.Keeper,
+	evmKeeper *evmkeeper.Keeper,
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+
+		//migrate rollapp params with missing min-gas-prices and updating drs to 2
+		err := rpKeeper.SetVersion(ctx, uint32(2))
+		if err != nil {
+			return nil, err
+		}
+		err = rpKeeper.SetMinGasPrices(ctx, rollappparamstypes.DefaultParams().MinGasPrices)
+		if err != nil {
+			return nil, err
+		}
+		//migrate evm params with missing gasDenom
+		evmOldParams := evmKeeper.GetParams(ctx)
+		evmOldParams.GasDenom = evmtypes.DefaultParams().GasDenom
+		err = evmKeeper.SetParams(ctx, evmOldParams)
+		if err != nil {
+			return nil, err
+		}
+		return mm.RunMigrations(ctx, configurator, fromVM)
+	}
+}
