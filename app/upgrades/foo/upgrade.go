f2c21962e60699621b127c855b064ae35ad8cfd2
danwt <30197399+danwt@users.noreply.github.com>
2025-01-02 16:17:33 +0000

    pre remove logger

--- app/upgrades/foo/upgrade.go
+++ app/upgrades/foo/upgrade.go
@@ -1,10 +1,13 @@
 package foo
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	hubgenkeeper "github.com/dymensionxyz/dymension-rdk/x/hub-genesis/keeper"
+	hubgenesistypes "github.com/dymensionxyz/dymension-rdk/x/hub-genesis/types"
 	"github.com/dymensionxyz/rollapp-evm/app/upgrades"
 	"github.com/tendermint/tendermint/libs/log"
 )
@@ -17,12 +20,22 @@ func CreateUpgradeHandler(
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		l := ctx.Logger().With("upgrade", UpgradeName)
 
-		migrateHubGenesis(ctx, l, kk.HubgenK)
+		if err := migrateHubGenesis(ctx, l, kk.HubgenK); err != nil {
+			return nil, fmt.Errorf("migrate hub genesis: %w", err)
+		}
 
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
 }
 
-func migrateHubGenesis(ctx sdk.Context, l log.Logger, k hubgenkeeper.Keeper) {
-	
+func migrateHubGenesis(ctx sdk.Context, l log.Logger, k hubgenkeeper.Keeper) error {
+	s := k.GetState(ctx)
+	s.OutboundTransfersEnabled = true
+	s.InFlight = false
+	s.HubPortAndChannel = &hubgenesistypes.PortAndChannel{
+		Port:    "transfer",
+		Channel: "channel-0",
+	}
+	k.SetState(ctx, s)
+	return k.PopulateGenesisInfo(ctx, nil)
 }
