84aa604882b207761cc7d924b0e1cecbd4234fe4
Daniel T <30197399+danwt@users.noreply.github.com>
2025-01-09 12:41:26 +0000

    feat: migration for mainnet rollapps (nim + mande) to upgrade to 3D (#442)

---
+++ app/upgrades/drs-5-from2d/upgrade_test.go
@@ -0,0 +1,55 @@
+package drs5from2d_test
+
+import (
+	"testing"
+
+	up "github.com/dymensionxyz/rollapp-evm/app/upgrades/drs-5-from2d"
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	"github.com/dymensionxyz/rollapp-evm/app"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/proto/tendermint/types"
+)
+
+func TestBeginBlocker(t *testing.T) {
+
+	app, _ := app.SetupWithOneValidator(t)
+	app.SetDymintVersionGetter(func() (uint32, error) {
+		return uint32(up.DRS), nil
+	})
+	h := int64(42)
+	ctx := app.NewUncachedContext(true, types.Header{
+		Height:  h - 1,
+		ChainID: "testchain_9000-1",
+	})
+	err := app.UpgradeKeeper.ScheduleUpgrade(ctx, upgradetypes.Plan{
+		Name:   up.UpgradeName,
+		Height: h,
+	})
+	require.NoError(t, err)
+
+	// simulate the upgrade process not panic.
+	require.NotPanics(t, func() {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Fatalf("Upgrade panicked: %v", r)
+			}
+		}()
+		// simulate the upgrade process
+		ctx = ctx.WithBlockHeight(h)
+		app.BeginBlocker(ctx, abci.RequestBeginBlock{})
+	})
+
+	// Check if the upgrade was applied correctly
+	state := app.HubGenesisKeeper.GetState(ctx)
+	require.True(t, state.OutboundTransfersEnabled, "OutboundTransfersEnabled should be true")
+	require.False(t, state.InFlight, "InFlight should be false")
+	require.NotNil(t, state.HubPortAndChannel, "HubPortAndChannel should not be nil")
+	require.Equal(t, "transfer", state.HubPortAndChannel.Port, "Port should be 'transfer'")
+	require.Equal(t, "channel-0", state.HubPortAndChannel.Channel, "Channel should be 'channel-0'")
+
+	p := app.RollappParamsKeeper.GetParams(ctx)
+	require.Equal(t, up.DRS, p.DrsVersion, "Version should be set to DRS")
+	require.Equal(t, up.DA, p.Da, "Version should be set to DRS")
+}
--- go.mod
+++ go.mod
@@ -10,8 +10,8 @@ require (
 	github.com/bcdevtools/evm-block-explorer-rpc-cosmos v1.1.4
 	github.com/cosmos/cosmos-sdk v0.46.16
 	github.com/cosmos/ibc-go/v6 v6.2.1
-	github.com/dymensionxyz/dymension-rdk v1.8.0-rc02
-	github.com/dymensionxyz/dymint v1.3.0-rc03
+	github.com/dymensionxyz/dymension-rdk v1.8.0-rc01.0.20250108162603-5198f720fca4
+	github.com/dymensionxyz/dymint v1.3.0-rc02.0.20250108131438-3789c4a7cd70
 	github.com/ethereum/go-ethereum v1.12.0
 	github.com/evmos/evmos/v12 v12.1.6
 	github.com/gogo/protobuf v1.3.3
