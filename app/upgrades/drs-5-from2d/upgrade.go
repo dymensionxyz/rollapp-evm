14347e0001d08b1c478da0220e87477be1c06f41
Daniel T <30197399+danwt@users.noreply.github.com>
2025-01-09 12:41:26 +0000

    feat: migration for mainnet rollapps (nim + mande) to upgrade to 3D (#442)

---
+++ app/upgrades/drs-5-from2d/upgrade.go
@@ -0,0 +1,63 @@
+package drs5from2d
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	hubgenkeeper "github.com/dymensionxyz/dymension-rdk/x/hub-genesis/keeper"
+	hubgenesistypes "github.com/dymensionxyz/dymension-rdk/x/hub-genesis/types"
+	rollappparamskeeper "github.com/dymensionxyz/dymension-rdk/x/rollappparams/keeper"
+	"github.com/dymensionxyz/rollapp-evm/app/upgrades"
+)
+
+func CreateUpgradeHandler(
+	kk upgrades.UpgradeKeepers,
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+
+		if err := migrateHubGenesis(ctx, kk.HubgenK); err != nil {
+			return nil, fmt.Errorf("migrate hub genesis: %w", err)
+		}
+
+		vmap, err := mm.RunMigrations(ctx, configurator, fromVM)
+		if err != nil {
+			return nil, fmt.Errorf("run migrations: %w", err)
+		}
+
+		// rollappparams is a new module so needs to go after RunMigrations to go after InitGenesis
+		if err := migrateRollappParams(ctx, kk.RpKeeper); err != nil {
+			return nil, fmt.Errorf("migrate rollapp params: %w", err)
+		}
+
+		return vmap, nil
+	}
+}
+
+func migrateHubGenesis(ctx sdk.Context, k hubgenkeeper.Keeper) error {
+	s := k.GetState(ctx)
+	s.OutboundTransfersEnabled = true
+	s.InFlight = false
+	s.HubPortAndChannel = &hubgenesistypes.PortAndChannel{
+		Port:    "transfer",
+		Channel: "channel-0",
+	}
+	k.SetState(ctx, s)
+	// Note: we won't populate genesis info. It has difficulties, such as missing checksum and denom metadata.
+	// It isn't needed for established chains.
+	return nil
+}
+
+func migrateRollappParams(ctx sdk.Context, k rollappparamskeeper.Keeper) error {
+	if err := k.SetVersion(ctx, DRS); err != nil {
+		return fmt.Errorf("set version: %w", err)
+	}
+	if err := k.SetDA(ctx, DA); err != nil {
+		return fmt.Errorf("set DA: %w", err)
+	}
+	// no need to set min gas prices, rollapp can do it when it likes
+	return nil
+}
