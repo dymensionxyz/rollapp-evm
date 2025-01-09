8134bde5f6785522a0912b4bc5f54e271811a41d
Daniel T <30197399+danwt@users.noreply.github.com>
2025-01-09 13:38:39 +0000

    feat: use rdk v1.8.0-rc02-3d-migration dymint v1.3.0-rc03-3d-migration (fixed) (#448)

---
+++ app/upgrades/drs-5-from2d/constants.go
@@ -0,0 +1,37 @@
+package drs5from2d
+
+import (
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	authztypes "github.com/cosmos/cosmos-sdk/x/authz"
+	"github.com/cosmos/cosmos-sdk/x/feegrant"
+	hubtypes "github.com/dymensionxyz/dymension-rdk/x/hub/types"
+	rollappparamstypes "github.com/dymensionxyz/dymension-rdk/x/rollappparams/types"
+	timeupgradetypes "github.com/dymensionxyz/dymension-rdk/x/timeupgrade/types"
+	claimstypes "github.com/evmos/evmos/v12/x/claims/types"
+
+	"github.com/dymensionxyz/rollapp-evm/app/upgrades"
+)
+
+const (
+	UpgradeName        = "drs-5-from2D"
+	DRS         uint32 = 5
+	DA          string = "celestia"
+)
+
+var Upgrade = upgrades.Upgrade{
+	Name:          UpgradeName,
+	CreateHandler: CreateUpgradeHandler,
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Deleted: []string{
+			claimstypes.ModuleName,
+			"denommetadata",
+		},
+		Added: []string{
+			authztypes.ModuleName,
+			feegrant.ModuleName,
+			timeupgradetypes.ModuleName,
+			hubtypes.ModuleName,
+			rollappparamstypes.ModuleName,
+		},
+	},
+}
