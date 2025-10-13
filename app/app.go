94a8123d2f5dcf5e02c6a5222760ec44c8b41740
Sergi Rene <sergi@dymension.xyz>
2025-10-13 13:24:32 +0200

    lint fix

--- app/app.go
+++ app/app.go
@@ -1125,7 +1125,7 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 	paramsKeeper.Subspace(rollappparamstypes.ModuleName)
 
 	// ethermint subspaces
-	paramsKeeper.Subspace(evmtypes.ModuleName).WithKeyTable(evmtypes.ParamKeyTable()) //nolint:SA1019: use MsgUpdateParams not supported yet on cosmos v0.46
+	paramsKeeper.Subspace(evmtypes.ModuleName).WithKeyTable(evmtypes.ParamKeyTable()) //nolint SA1019 use MsgUpdateParams not supported yet on cosmos v0.46
 	paramsKeeper.Subspace(feemarkettypes.ModuleName).WithKeyTable(feemarkettypes.ParamKeyTable())
 	// evmos subspaces
 	paramsKeeper.Subspace(erc20types.ModuleName)
